8afa60b1ec0bd4d691d4000c5e91811f0e0e3661
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-12-21 22:24:47 +0300

    feat: add tabs to map, add full-screen button, do some refactoring

--- src/components/map/mapInit.component.js
+++ src/components/map/mapInit.component.js
@@ -7,24 +7,9 @@ async function initMap(url) {
   };
   const response = await fetch(url, requestOptions);
   const data = await response.json();
-  const map = new Map('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png');
-  map.addLayout();
   const countries = data;
-  countries.forEach((country) => {
-    const countryName = country.country;
-    const { population } = country;
-    const totalCases = country.cases;
-    const totalDeaths = country.deaths;
-    const currLat = country.countryInfo.lat;
-    const currLon = country.countryInfo.long;
-    let intensity = (totalCases / population) * 1000;
-    if (intensity === Infinity) {
-      intensity = 1;
-    }
-    map.putPoint(currLat, currLon, '#cc22f1', '#ec0b2b', intensity, intensity);
-    map.showPopup(`<div class="map__country-name">${countryName}</div>
-    <div class="map__total-cases">Total cases: ${totalCases}</div><div class="map__total-deaths">Total deaths: ${totalDeaths}</div>`);
-  });
+  const map = new Map('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png');
+  map.init(countries);
 }
 
 export default initMap;
