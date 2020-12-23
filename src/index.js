4f02ec7bbaf132a8bcc1317a3af7f54317dd9937
Alexus-bat <alexey.bond97@gmail.com>
2020-12-23 10:55:43 +0300

    fix: add class table

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './components/table/table.main.scss';
 import sayHi from './components/some-component/component';
 
 import getData from './components/table/table.main';
-import toggle from './components/table/table.switch';
 
 sayHi();
 
 getData();
-
-toggle();
